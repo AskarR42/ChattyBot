7922662c64b18d4dac72020ea2841e50e629ff78
askar <askar.rodin.42@gmail.com>
2021-04-12 15:56:57 +0300

    greeting message

---
+++ src/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!  My name is Eliss");
+        System.out.println("I was created 2021.04.12");
+    }
+}
