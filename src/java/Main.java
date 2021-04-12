dda38adb89b751a1c41217aff14c399443319304
askar <askar.rodin.42@gmail.com>
2021-04-12 17:17:06 +0300

    It can remember your name now!

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello!  My name is Eliss");
         System.out.println("I was created 2021.04.12");
+        System.out.println("Please, remember me your name");
+
+        String name = scanner.next();
+        System.out.println("What a great name you have " + name + "!");
     }
 }
