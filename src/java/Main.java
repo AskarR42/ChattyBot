24a7ee2beed0236ced8dd84296b40f8f04b70a3c
askar <askar.rodin.42@gmail.com>
2021-04-13 13:26:56 +0300

    It can guess your age

--- src/java/Main.java
+++ src/java/Main.java
@@ -10,5 +10,17 @@ public class Main {
 
         String name = scanner.next();
         System.out.println("What a great name you have " + name + "!");
+
+
+        System.out.println("Let me guess your age");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7");
+
+        int reminder3 = scanner.nextInt();
+        int reminder5 = scanner.nextInt();
+        int reminder7 = scanner.nextInt();
+
+        int age = (reminder3 * 70 + reminder5 * 21 + reminder7 * 15) % 105;
+
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
     }
 }
