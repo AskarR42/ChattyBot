b39b5a02dac52393ffea26ccc109b5cccd45a387
askar <askar.rodin.42@gmail.com>
2021-04-13 20:53:15 +0300

    Completed

--- src/java/SimpleBot.java
+++ src/java/SimpleBot.java
@@ -4,7 +4,7 @@ public class SimpleBot {
     final static Scanner scanner = new Scanner(System.in); // Do not change this line
 
     public static void main(String[] args) {
-        greet("Aid", "2018"); // change it as you need
+        greet("Eliss", "2021"); // change it as you need
         remindName();
         guessAge();
         count();
